072ac74a93168293ad0d4d958dfde868aeccf8ef
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-01-18 15:51:05 -0500

    Controller/routing now functional for all pages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -8,12 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongod://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
-require("./controller/controller.js");
+
+const controller = require("./controller/controller.js");
+app.use("/", controller);
 
 app.listen(PORT, () => {
     console.log(`Application is now running on port ${PORT}!`);
